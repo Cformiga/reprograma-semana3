97d750f78e426efe2c45d79cbc3719ab0678a18b
Cformiga <cformiga@gmail.com>
2021-05-27 09:48:25 -0300

    resoluçaoquestao2

---
+++ exercicio2.js
@@ -0,0 +1,11 @@
+//2 - crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+
+function horaParaDia(quantidade){
+    let hora = 24
+    let resultado = quantidade * hora
+    return resultado
+}
+
+console.log(horaParaDia(3))
+
+}
